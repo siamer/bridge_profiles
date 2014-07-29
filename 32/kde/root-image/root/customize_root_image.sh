0099dd91049854e8a6b50d80753b037c5facd106
root <root@localhost.localdomain>
2014-07-28 20:09:45 -0500

    first commit

---
+++ 32/kde/root-image/root/customize_root_image.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e -u
+
+sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d/58-lcdfilter.conf
+
+usermod -s /bin/bash root
+cp -aT /etc/skel/ /root/
+
+useradd -m -k /etc/skel -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash live
+
+chmod 440 /etc/sudoers
+
+sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
+
+systemctl enable multi-user.target pacman-init.service NetworkManager.service choose-mirror.service
---
+++ 64/xfce/root-image/etc/skel/.bash_profile
@@ -0,0 +1,2 @@
+. $HOME/.bashrc
+[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx
